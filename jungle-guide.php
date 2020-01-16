1f0af8398dd684b61f85f635f9aa7dcdf72c7a2f
Azat <azat@rahmanow.com>
2020-01-16 04:46:26 +0100

    - Added wordpress maintenance plugin file

--- jungle-guide.php
+++ jungle-guide.php
@@ -1,6 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+/**
+ * Plugin Name: Jungle Coming Soon page
+ * Plugin URI: https://www.jungle.guide/
+ * Description: Jungle Coming soon or maintenance plugin.
+ * Version: 1.0
+ * Author: Azat Rahmanov
+ * Author URI: http://rahmanow.com/
+ **/
 
+/* Tell search engines that the site is temporarily unavailable */
+
+$protocol = $_SERVER["SERVER_PROTOCOL"];
+if ( 'HTTP/1.1' != $protocol && 'HTTP/1.0' != $protocol ) $protocol = 'HTTP/1.0';
+header( "$protocol 503 Service Unavailable", true, 503 );
+header( 'Content-Type: text/html; charset=utf-8' );
+header( 'Retry-After: 600' );
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -8,25 +25,15 @@
     <meta name="author" content="">
     <link rel="icon" href="">
     <title>Jungle - Coming Soon</title>
-    
-    <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="assets/bootstrap.min.css" type="text/css">
-
-    <!-- Place your stylesheet here-->
     <link href="assets/main.css" rel="stylesheet" type="text/css">
-    <style>
-
-
-    </style>
 </head>
-
 <body class="body">
-
-<div class="cursor" id="cursor">
-    <div class="cursor__outer">
-        <img class="cursor__ring" src="assets/jungle_cursor.png" alt="Coming soon">
+    <div class="cursor" id="cursor">
+        <div class="cursor__outer">
+            <img class="cursor__ring" src="assets/jungle_cursor.png" alt="Coming soon">
+        </div>
     </div>
-</div>
 
     <main role="main" class="container-fluid">
         <div class="row">
@@ -35,7 +42,6 @@
                     <h1 class="heading-4" onclick="openNav()">?</h1>
                 </div>
 
-                <!-- Large modal -->
                 <div id="jungleSide" class="sidepanel">
                     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&larr;</a>
                     <div class="row jg-row ml-">
@@ -43,63 +49,35 @@
                             Junge ist eine neue Agentür für digitale Kommunikation. Wir helfen Marken und Unternehmen dabei ihren Weg durch den digitalen Dschungel zu finden.
                         </p>
                         <a href="mailto:hi@jungle.guide">Haben wir interesse geweckt? <br /> Dann sprechen Sie uns an.</a>
-
-
                     </div>
-
                     <div class="row jg-row-1">
                         <span>t: 0176 3213 7779</span>
                         <span>m: hi@jungle.guide </span>
                     </div>
                 </div>
 
-
             </div>
+
             <div class="col-8">
 
             </div>
 
             <div class="col-2">
+
             </div>
         </div>
 
         <div class="row align-items-center row-welcome">
             <div class="col-12 Iam">
-
-
-
-
-                    <h1 class="heading-3">Welcome to</h1>
-                    <b>
-                        <div class="innerIam">
-                             the Jungle <br />
-                    Social Media <br />
-                    digital Branding <br />
-                    digital Strategy
-                        </div>
-                    </b>
-
-
-
-
-
-               <!-- <h1 class="heading-3">Welcome to</h1>
-                    <div class="jg-row-3">
-                <h3 class="h3 heading-3 innerIam">
-                    the Jungle <br />
-                    Social Media <br />
-                    digitalBranding <br />
-                    digital Strategy
-                </h3>
-                    </div>-->
-
-
-
-                    <!--<span class="letters letters-1">the Jungle</span>
-                    <span class="letters letters-2">Social Media</span>
-                    <span class="letters letters-3">digital Branding</span>
-                    <span class="letters letters-4">digital Strategy</span>
-                </h3>-->
+                <h1 class="heading-3">Welcome to</h1>
+                <b>
+                    <div class="innerIam">
+                        the Jungle <br />
+                        Social Media <br />
+                        digital Branding <br />
+                        digital Strategy
+                    </div>
+                </b>
             </div>
         </div>
 
@@ -110,18 +88,10 @@
                 </div>
             </div>
         </div>
-       <!-- <div class="hero"></div>-->
-
-        <!-- <script src="wf.js" type="text/javascript"></script>-->
-    
-            </main><!-- /.container -->
 
 
-    <!-- Bootstrap core JavaScript
-            <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    </main>
 
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
     <script src="assets/jquery-3.4.1.min.js"></script>
     <script src="assets/jquery-3.4.1.slim.min.js"></script>
     <script src="assets/popper.min.js"></script>
@@ -155,9 +125,13 @@
             document.getElementById("jungleSide").style.width = "0";
             document.getElementById("cursor").style.display = "inline";
         }
-
-
     </script>
 
 </body>
-</html>
+
+<?php
+/* This passes control back to the wordpress upgrade routine */
+die();
+/* Don't change this */
+?>
+
